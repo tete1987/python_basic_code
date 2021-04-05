99dcc7e2835b2a2d71f7c1184d11337cc3b17eae
tete1987 <69782329+tete1987@users.noreply.github.com>
2021-04-05 22:20:21 +0800

    Delete aaa.py

--- aaa.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*- 
-# @Time : 2021/3/1 8:50 
-# @Author : TETE
-# @File : aaa.py 
-for i in range(1,10):
-    for j in range(1,i+1):
-        print("%d * %d = %2d "%(j,i,j*i),end= ' ')
-    print("")
